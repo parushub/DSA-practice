ab1d89a1e2859771cf995e1548649d9dbe98c734
parushub <97519034+parushub@users.noreply.github.com>
2025-07-09 00:10:02 +0530

    Create Subsets.cpp
    Given an integer array nums of unique elements, return all possible subsets (the power set).
    
    The solution set must not contain duplicate subsets. Return the solution in any order.

---
+++ Subsets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void allsubsets(vector<int> &arr, vector<int> &ans, int i, vector<vector<int>> &powersets)
+    {
+        if(i==arr.size())
+        {
+            powersets.push_back({ans});
+            return;
+        }
+
+        //include
+        ans.push_back(arr[i]);
+        allsubsets(arr,ans,i+1, powersets);
+
+        //exclude
+        ans.pop_back(); //backtrack
+        allsubsets(arr,ans,i+1, powersets);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> powersets;
+        vector<int> ans;
+
+        allsubsets(nums, ans, 0, powersets);
+        return powersets;
+    }
+};
